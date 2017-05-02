1478a93d862734e1cc458f66da01e9094cbd621a
alex <alexandrosrotsides@gmail.com>
2017-05-02 22:22:34 +0100

    added fullscreen support

--- app/src/main/java/com/alexs7/percentof/MainActivity.java
+++ app/src/main/java/com/alexs7/percentof/MainActivity.java
@@ -1,12 +1,26 @@
 package com.alexs7.percentof;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else{
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
